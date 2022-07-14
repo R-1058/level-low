f97866a7ccb7ae16a64be5597d1efd900040768f
ARAVINDHAN G <A1354@SVSGLOBAL.COM>
2022-07-14 14:36:39 +0530

    Negative Scenario

--- src/main/java/org/run/Old_Vehicle.java
+++ src/main/java/org/run/Old_Vehicle.java
@@ -57,7 +57,9 @@ public class Old_Vehicle   {
 		 
 		WebDriver driver = lb.launchBrowser("chrome");
 		
-		lb.loadUrl("https://experience.novactech.in/digital/sgi/fwl/login.html");
+		lb.loadUrl("https://www.facebook.com/");
+		
+//		https://experience.novactech.in/digital/sgi/fwl/login.html
 		
 //		https://experience.novactech.in/digital/sgi/fwl/login.html
 		
